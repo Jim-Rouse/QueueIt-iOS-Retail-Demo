c3a5dad3231f4f4fdb3853daf74cee72213d452f
Rouser <17075090+Jim-Rouse@users.noreply.github.com>
2026-02-25 14:15:47 -0600

    Initial Commit

---
+++ QueueItRetailDemo/QueueItRetailDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueItRetailDemoApp.swift
+//  QueueItRetailDemo
+//
+//  Created by James Rouse on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct QueueItRetailDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
